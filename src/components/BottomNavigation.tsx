jmstechnologiesinc/react-native-paper

commit 86d897699c5a99bb53643c738b688d59265c2137

fix: grey screen on first open of tab on react-native-web (#2280)

src/components/BottomNavigation.tsx
@@ -271,7 +271,7 @@ const MIN_RIPPLE_SCALE = 0.001; // Minimum scale is not 0 due to bug with animat
 const MIN_TAB_WIDTH = 96;
 const MAX_TAB_WIDTH = 168;
 const BAR_HEIGHT = 56;
-const FAR_FAR_AWAY = 9999;
+const FAR_FAR_AWAY = Platform.OS === 'web' ? 0 : 9999;
 
 const Touchable = ({
   route: _0,
@@ -731,7 +731,15 @@ class BottomNavigation extends React.Component<Props, State> {
                   Platform.OS === 'ios' ? navigationState.index !== index : true
                 }
               >
-                <Animated.View style={[styles.content, { top }]}>
+                <Animated.View
+                  style={[
+                    styles.content,
+                    { top },
+                    Platform.OS === 'web'
+                      ? { display: loaded.includes(index) ? 'flex' : 'none' }
+                      : null,
+                  ]}
+                >
                   {renderScene({
                     route,
                     jumpTo: this.jumpTo,

commit 06ac4282eddfa6183a68a9a96d578d0e60492fee

fix: fix BottomNavigation bug introduced in https://github.com/callstack/react-native-paper/pull/2280

src/components/BottomNavigation.tsx
@@ -737,9 +737,7 @@ class BottomNavigation extends React.Component<Props, State> {
                     { top },
                     Platform.OS === 'web'
                       ? {
-                          display: loaded.includes(index.toString())
-                            ? 'flex'
-                            : 'none',
+                          display: loaded.includes(route.key) ? 'flex' : 'none',
                         }
                       : null,
                   ]}